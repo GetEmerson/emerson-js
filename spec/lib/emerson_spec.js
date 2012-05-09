bfb0460bf13f6fd6e04173807bf4f590d34c42ef
Corey Innis <corey@coolerator.net>
2012-05-08 19:41:38 -0700

    add Emerson.init
    so far, just a skeleton.  will be the mechanism by which applications enable emerson and to initial setup/activation.

--- spec/lib/emerson_spec.js
+++ spec/lib/emerson_spec.js
@@ -6,4 +6,16 @@ describe("Emerson", function() {
   it("is the expected version", function() {
     expect(Emerson.VERSION).toEqual('0.0.0');
   });
+
+  describe(".init", function() {
+    it("calls module.init for registered modules", function() {
+      _.each(['util', 'view'], function(mod) {
+        spyOn(Emerson[mod], 'init');
+      });
+
+      Emerson.init();
+      expect(Emerson.util.init).toHaveBeenCalled();
+      expect(Emerson.view.init).toHaveBeenCalled();
+    });
+  });
 });
