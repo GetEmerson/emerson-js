990d1d75413fbe858a338815a09c97532567b014
Corey Innis <corey@coolerator.net>
2012-05-10 09:51:50 -0700

    version bump: 0.0.2

--- spec/lib/emerson/base_spec.js
+++ spec/lib/emerson/base_spec.js
@@ -1,10 +1,6 @@
 describe("Emerson", function() {
-  it("is defined", function() {
-    expect(Emerson).toBeDefined();
-  });
-
   it("is the expected version", function() {
-    expect(Emerson.VERSION).toEqual('0.0.1');
+    expect(Emerson.VERSION).toEqual('0.0.2');
   });
 
   describe(".config", function() {
