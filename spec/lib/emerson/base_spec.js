79d384a64044860fd053f93f9d723febffdace14
Corey Innis <corey@coolerator.net>
2012-10-16 18:11:36 -0700

    version 0.0.10

--- spec/lib/emerson/base_spec.js
+++ spec/lib/emerson/base_spec.js
@@ -1,6 +1,6 @@
 describe("Emerson", function() {
   it("is the expected version", function() {
-    expect(Emerson.VERSION).toEqual('0.0.9');
+    expect(Emerson.VERSION).toEqual('0.0.10');
   });
 
   describe(".config", function() {
